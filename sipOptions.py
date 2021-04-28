b3000212fc480eb0761302efa8e1cab593b477d0
PiotrPosiadala <piotr.posiadala.stud@pw.edu.pl>
2021-04-28 18:32:34 +0200

    Add text (#1)
    * Dodano przesylane dane
    
    * Wersja do testow
    
    * Lekkie poprawki
    
    * Kolejna mala poprawka
    
    * Kolejna mala poprawka
    
    * Poprawka
    
    * Poprawka
    
    * Poprawka
    
    * Poprawka v21321
    
    * Wersja dzialajaca, ale z ==
    
    * Poprawiony detector
    
    * Wrzucam przykladowy pcap
    
    * Plik .exe detektora
    
    * args and ports change
    
    Co-authored-by: LaskowskiMichal98 <laskowski.michal.44@gmail.com>
    Co-authored-by: Michał Laskowski <m.laskowski@stud.elka.pw.edu.pl>

--- sipOptions.py
+++ sipOptions.py
@@ -1,18 +1,18 @@
+import base64
+import random
+import time
 from scapy.all import *
 import argparse
 import secrets as s
-import time
+from scapy.layers.inet import UDP
+from scapy.layers.inet import IP
 
-'''
-sipOptions
-Simple SIP agent sending SIP Options messages 
-containing or not coverted informations
-'''
 
 def sip_options():
     '''
-    SIP Options message witout covert channel
+    SIP Options message without covert channel
     '''
+    rand_list = [22, 25, 28]
     return (
         'OPTIONS sip:terminal01121605@pw.edu.pl;transport=udp SIP/2.0\r\n'
         'Via: SIP/2.0/UDP sbcwaw1@pw.edu.pl;branch={0}\r\n'
@@ -21,13 +21,13 @@ def sip_options():
         'Call-ID: {2}@pw.edu.pl\r\n'
         'CSeq: {3} OPTIONS\r\n'
         'Max-Forwards: 70\r\n'
-        'Call-Info: Please, let me know about your avability\r\n'
+        'Call-Info: Please, let me know about your avability.\r\n'
         'Organization: Warsaw University of Technology, Faculty of Electronics and Information Technology \r\n'
-        'Content-Length: 0\r\n\r\n').format(s.token_urlsafe(8), s.token_urlsafe(8), s.token_urlsafe(24), s.randbits(16))
+        'Content-Length: 0\r\n\r\n').format('z9hG4bK' + s.token_urlsafe(5), s.token_urlsafe(8), s.token_urlsafe(random.choice(rand_list)), s.randbits(16))
 
 
-def stegano_sip_options(gen):
-    ''' 
+def stegano_sip_options(msg):
+    '''
     SIP Options message with covert channel
     '''
     return (
@@ -38,38 +38,51 @@ def stegano_sip_options(gen):
         'Call-ID: {2}@pw.edu.pl\r\n'
         'CSeq: {3} OPTIONS\r\n'
         'Max-Forwards: 70\r\n'
-        'Call-Info: Please, let me know about your avability\r\n'
-        'Organization: {4} \r\n'
-        'Content-Length: 0\r\n\r\n').format(s.token_urlsafe(8), s.token_urlsafe(8), s.token_urlsafe(24), s.randbits(16), next(gen))
-
-def get_line(file_name):
-    '''
-    Receiving next line of the file every time this generator is called
-    '''
-    for line in open(file_name, "r"):
-        yield line
+        'Call-Info: Please, let me know about your availability\r\n'
+        'Organization: Warsaw University of Technology, Faculty of Electronics and Information Technology \r\n'
+        'Content-Length: 0\r\n\r\n').format('z9hG4bK' + msg[:5], s.token_urlsafe(8), msg[5:], s.randbits(16))
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dst", default="192.168.0.11", help="The destination IP")
-parser.add_argument("-s", "--src", default="192.168.0.11", help="The source IP")
-parser.add_argument("-c", "--covert", action="store_true", default=False, help="Generating SIP Options with hidden info (covert channel)")
-parser.add_argument("-f", "--file", default="message.txt", help="File to hide in covert channel")
-parser.parse_args()
-args = parser.parse_args()
+def go_go_sip(args):
+    dst_ip = args.dst
+    src_ip = args.src
+    covert = args.covert
+    file_name = args.file
+    src_port, dst_port = 5060, 5060  # 5060 is default SIP port
+    file = open(file_name, "r")
+    data = file.read()
 
-dst_ip = args.dst
-src_ip = args.src
-covert = args.covert
-file_name = args.file
+    for i in range(0, len(data), 20):
+        msg = base64.b64encode(bytes(data[i:i + 20], 'utf-8'))
+        msg = msg.decode('utf-8')
 
-src_port, dst_port = 5060, 5061    # 5060 is deafult SIP port
-line_gen = get_line(file_name)     # generator for reading lines of file
+        # if msg[-2:] == "==":
+        #     msg = msg[0:-2]
+        # elif msg[-1:] == "=":
+        #     msg = msg[0:-1]
+
+        if covert:
+            pkt = IP(src=src_ip, dst=dst_ip) / UDP(sport=src_port, dport=dst_port) / stegano_sip_options(msg)
+        else:
+            pkt = IP(src=src_ip, dst=dst_ip) / UDP(sport=src_port, dport=dst_port) / sip_options()
+        send(pkt)
+        time.sleep(0.4)
+
+    file.close()
 
 
-if covert: 
-    pkt = IP(src=src_ip, dst=dst_ip)/UDP(sport=src_port, dport=dst_port)/stegano_sip_options(line_gen)
-else:
-    pkt = IP(src=src_ip, dst=dst_ip)/UDP(sport=src_port, dport=dst_port)/sip_options()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dst", default="192.168.0.11", help="The destination IP")
+    parser.add_argument("-s", "--src", default="192.168.0.11", help="The source IP")
+    parser.add_argument("-c", "--covert", action="store_true", default=False,
+                        help="Generating SIP Options with hidden info (covert channel)")
+    parser.add_argument("-f", "--file", default="message.txt", help="File to hide in covert channel")
+    parser.parse_args()
+    args = parser.parse_args()
 
-send(pkt)
+    go_go_sip(args)
+
+
+if __name__ == '__main__':
+    main()
